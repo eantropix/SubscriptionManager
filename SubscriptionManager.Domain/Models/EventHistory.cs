1186ca41efcab5522a474a86b3a4850df3efea11
Eantropix <eantropixdev@outlook.com>
2023-01-20 15:32:51 -0300

    Added EventHistory Domain

--- SubscriptionManager.Domain/Models/EventHistory.cs
+++ SubscriptionManager.Domain/Models/EventHistory.cs
@@ -8,7 +8,7 @@ namespace SubscriptionManager.Domain.Models
 {
     public class EventHistory
     {
-        public int Id {  get; set; }
+        public int Id { get; set; }
         public int SubscriptionId { get; set; }
         public string Type { get; set; }
         public DateTime CreatedAt { get; set; }
