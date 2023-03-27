c6e00485aacd0e0b4b85a3cd92d9275664eb1b77
Eantropix <eantropixdev@outlook.com>
2023-03-27 10:40:03 -0300

    Created Entity Configurations and DbContext

---
+++ SubscriptionManager.Domain/Context/EntityConfigurations/SubscriptionEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SubscriptionManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Context.EntityConfigurations
+{
+    public class SubscriptionEntityConfiguration : IEntityTypeConfiguration<Subscription>
+    {
+        public SubscriptionEntityConfiguration() { }
+
+        public void Configure(EntityTypeBuilder<Subscription> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Property(x => x.StatusId).IsRequired();
+            builder.Property(x => x.UserId).IsRequired();
+        }
+    }
+}
