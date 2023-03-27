47a2678442c4d2c14561ae17e0d21674f5d151d5
Eantropix <eantropixdev@outlook.com>
2023-03-27 10:43:46 -0300

    Removed example controller, started user controller, added Infrastructure project file

---
+++ Subscription.Api/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SubscriptionManager.Domain.Models;
+
+namespace SubscriptionManager.Api.Controllers
+{
+    [ApiController]
+    [Route ("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        public UserController()
+        {
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            return Ok();
+        }
+    }
+}
--- Subscription.Api/Program.cs
+++ Subscription.Api/Program.cs
@@ -1,3 +1,10 @@
+using Domain.Context;
+using Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SubscriptionManager.Domain.Models;
+using SubscriptionManager.Domain.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
+
+builder.Services.AddScoped<IRepository<EventHistory>, EventHistoryRepository>();
+builder.Services.AddScoped<IRepository<Status>, StatusRepository>();
+builder.Services.AddScoped<IRepository<Subscription>, SubscriptionRepository>();
+builder.Services.AddScoped<IRepository<User>, UserRepository>();
+
 
 var app = builder.Build();
 
