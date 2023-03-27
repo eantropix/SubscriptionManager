9bca8cee9842e985c2bf410b97c49cfcc58b30a3
Eantropix <eantropixdev@outlook.com>
2023-03-27 10:42:47 -0300

    Edited launchSettings, added UnitOfWork and started AppService for User

---
+++ Application/Services/UserAppService.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces.Services;
+using SubscriptionManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class UserAppService : IUserAppService
+    {
+        public UserAppService()
+        {
+            
+        }
+
+        public Task CreateUser(User user) {
+            throw new NotImplementedException();
+        }
+
+        public int ReturnZero() { return 0; }
+    }
+}
---
+++ Infrastructure/Infrastructure/Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.UnitOfWork {
+    public class UnitOfWork {
+
+        private readonly DbContext _Context;
+
+        public UnitOfWork(DbContext context) {
+            _Context = context;
+        }
+
+        public bool Commit() {
+            var rows = _Context.SaveChanges();
+            return (rows > 0);
+        }
+        public async Task<bool> CommitAsync() {
+            var rows = await _Context.SaveChangesAsync();
+            return (rows > 0);
+        }
+
+        public void Dispose() {
+            _Context.Dispose();
+        }
+    }
+}
