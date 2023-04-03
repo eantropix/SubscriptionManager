e5396aec26efe66c282ca25c6c09031593cb10f9
Eantropix <eantropixdev@outlook.com>
2023-04-03 14:47:51 -0300

    Added message queues with RabbitMQ and started Application layer

---
+++ Application/Interfaces/Services/IMessageBrokerAppService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services
+{
+    public interface IMessageBrokerAppService<T> where T : class
+    {
+        void Publish(T body, string routeKey = "");
+    }
+}
---
+++ Application/Interfaces/Services/IUserAppService.cs
@@ -0,0 +1,9 @@
+﻿using SubscriptionManager.Domain.Models;
+
+namespace Application.Interfaces.Services
+{
+    public interface IUserAppService : IMessageBrokerAppService<User>
+    {
+        void Create(User user);
+    }
+}
